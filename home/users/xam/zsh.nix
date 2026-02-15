dbc6851260f333822e5558e0424fddce74b60cd5
xam <m.porseryd@gmail.com>
2026-02-15 01:09:35 +0100

    updated to 25.11

--- home/users/xam/zsh.nix
+++ home/users/xam/zsh.nix
@@ -15,49 +15,68 @@
 
   initContent = ''
     # setting up and binding tmux sessionizer
+    
     tmux_sessionizer_widget() {
       zle -I                     
       tmux-sessionizer          
     }
+
     zle -N tmux_sessionizer_widget
     bindkey '^f' tmux_sessionizer_widget
     
+    
     # loading ssh keys
+    
     if [ -z "$SSH_AUTH_SOCK" ]; then
       eval "$(ssh-agent -s)" > /dev/null
       ssh-add ~/.ssh/lnu_ed25519 2>/dev/null
     fi
     
     # automatically start a tmux session when opening an shell if apropriate
+    
     if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
       exec tmux
     fi
     
+    # defined function for building and commiting nixos config
     build_with_commit() {
-    echo "$(date '+%H:%M:%S'): Starting rebuild"
     
-    ret=$(pwd)
-    cd ~/nixos/
+      echo "$(date '+%H:%M:%S'): Starting rebuild"
     
-    if sudo -E nixos-rebuild switch --flake .# --impure ; then
-      echo "$(date '+%H:%M:%S'): Rebuild successfull"
+      ret=$(pwd)
+      cd ~/nixos/
     
-      print -n "Commit message: "
-      read msg
-    
-      if [ -z "$msg" ]; then
-        msg="Update $(date '+%Y-%m-%d %H:%M:%S')"
+      if sudo -E nixos-rebuild switch --flake .# --impure ; then
+	echo "$(date '+%H:%M:%S'): Rebuild successfull"
+	read -p "Commit and push? (y/n) " -n 1 -r 
+	
+	
+  
+	echo
+
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+	  print -n "Commit message: "
+	  read msg
+	
+	  if [ -z "$msg" ]; then
+	    msg="Update $(date '+%Y-%m-%d %H:%M:%S')"
+	  fi
+          
+	  git add .
+	  git commit -am "$msg"
+	  echo "Committed: '$msg'"
+	  
+	  git push
+	  echo "$(date '+%H:%M:%S'): Pushed"
+	else
+	  echo "Skipped commit"
+	fi
+      else
+	echo "$(date '+%H:%M:%S'): Rebuild failed. No changes committed."
       fi
     
-      git add .
-      git commit -am "$msg"
-      echo "Committed: '$msg'"
-      git push
-      echo "$(date '+%H:%M:%S'): Pushed"
-    else
-      echo "$(date '+%H:%M:%S'): Rebuild failed. No changes committed."
-    fi
-    cd $ret
+      cd $ret
+    }
     
     
     alias rebuild="rebuild_with_commit"
