5cda3c8747f75d4c239f91ee306804e0fe70293c
xam <m.porseryd@gmail.com>
2026-03-18 01:33:48 +0100

    Update 2026-03-18 01:33:48

--- home/users/xam/xam.nix
+++ home/users/xam/xam.nix
@@ -1,22 +1,5 @@
 { config, pkgs, ... }:
 
-
-let
-  agentEnv = pkgs.buildEnv {
-    name = "opencode-agent-env";
-    paths = with pkgs; [
-      neovim
-      luacheck
-      stylua
-      lua
-      rustup
-      gcc
-      git
-      fd
-      make
-    ];
-  };
-in
 {
   imports = [
     ./xam_stylix.nix
@@ -40,6 +23,7 @@ in
       PATH = "$HOME/.local/bin:$PATH";
     };
 
+
     packages = with pkgs; [
       #teamspeak3
       ollama
@@ -55,24 +39,24 @@ in
 
       bubblewrap
       (writeShellScriptBin "opencode" ''
-	exec ${pkgs.bubblewrap}/bin/bwrap \
-	  --ro-bind /nix/store /nix/store \
-       	  --ro-bind /run/current-system /run/current-system \
-       	  --ro-bind /etc /etc \
-       	  --proc /proc \
-       	  --dev-bind /dev /dev \
-       	  --tmpfs /tmp \
-       	  --tmpfs /home/xam \
-       	  --bind ''${HOME}/Projects ''${HOME}/Projects \
-       	  --ro-bind ''${HOME}/nixos ''${HOME}/nixos \
-       	  --bind ''${HOME}/.config/opencode ''${HOME}/.config/opencode \
-       	  --bind ''${HOME}/.local/state/opencode ''${HOME}/.local/state/opencode \
-       	  --bind ''${HOME}/.local/share/opencode ''${HOME}/.local/share/opencode \
-       	  --setenv HOME /home/xam \
-       	  --unshare-ipc \
-       	  --unshare-uts \
-       	  --die-with-parent \
-       	  ${pkgs.opencode}/bin/opencode "$@"
+      exec ${pkgs.bubblewrap}/bin/bwrap \
+        --ro-bind /nix/store /nix/store \
+        --ro-bind /run/current-system /run/current-system \
+        --ro-bind /etc /etc \
+        --proc /proc \
+        --dev-bind /dev /dev \
+        --tmpfs /tmp \
+        --tmpfs /home/xam \
+        --bind ''${HOME}/Projects ''${HOME}/Projects \
+        --ro-bind ''${HOME}/nixos ''${HOME}/nixos \
+        --bind ''${HOME}/.config/opencode ''${HOME}/.config/opencode \
+        --bind ''${HOME}/.local/state/opencode ''${HOME}/.local/state/opencode \
+        --bind ''${HOME}/.local/share/opencode ''${HOME}/.local/share/opencode \
+        --setenv HOME /home/xam \
+        --unshare-ipc \
+        --unshare-uts \
+        --die-with-parent \
+        ${pkgs.opencode}/bin/opencode "$@"
       '')
 
       (writeShellScriptBin "tmux-sessionizer" (builtins.readFile /home/xam/nixos/scripts/tmux-sessionizer))
