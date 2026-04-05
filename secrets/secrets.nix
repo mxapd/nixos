fff0ffb765c5493c59218a609d36a0935a71fabf
xam <m.porseryd@gmail.com>
2026-04-05 21:58:47 +0200

    wip fixing hermes-agent

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,12 +2,11 @@ let
   # root nixos desktop
   nixos_desktop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAwA0Qn2WZa+mz79ehNsLBOj2uV+dTJOuoLduxsOk+bk";
   hermes = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO8Jgq6sCmPA7WcZttZQ5JOxV09JLsPfhfooOUQSsJHf";
-  xam = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF77VKFOVMZT+yWeANJPddiMk4vw1ocU/aQFAeKZ0pOk";
 in
 {
   "github_ssh_mxapd.age".publicKeys = [ nixos_desktop ];
   "gitlab_ssh_lnu.age".publicKeys = [ nixos_desktop ];
   "gitea_ssh_ancient.age".publicKeys = [ nixos_desktop ];
   "gitea_ssh_swahnlabs.age".publicKeys = [ nixos_desktop ];
-  "hermes-env.age".publicKeys = [ hermes xam ];
+  "hermes-env.age".publicKeys = [ hermes ];
 }
