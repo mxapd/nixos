1ba3b5797a3e3a17bc4606217c374901b38c6fbd
xam <xam@example.com>
2026-06-02 12:26:35 +0200

    double declaration in tailscale.nix

--- core/tailscale.nix
+++ core/tailscale.nix
@@ -7,21 +7,16 @@
     #"100.64.0.Y" = [ "hermes" ];
     "100.64.0.6" = [ "desktop" ];
     "100.64.0.7" = [ "laptop" ];
-  };
 
+
+    "100.64.0.17" = ["nextcloud.yggdrasil.com" "firefly.yggdrasil.com" "importer.yggdrasil.com" "gitea.yggdrasil.com"];
+  };
 
   services.tailscale = {
     enable = true;
     useRoutingFeatures = "client";
   };
   
-  networking = {
-    hosts = {
-      # Swahnlabs
-      "100.64.0.17" = ["nextcloud.yggdrasil.com" "firefly.yggdrasil.com" "importer.yggdrasil.com" "gitea.yggdrasil.com"];
-    };
-  };
-
   environment.systemPackages = with pkgs; [
     tailscale
   ];
