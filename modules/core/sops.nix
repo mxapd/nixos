cfab06be65632091fae3d08e028ad9f6668a5298
xam <xam@example.com>
2026-07-09 19:24:07 +0200

    Update 2026-07-09 19:24:07

--- modules/core/sops.nix
+++ modules/core/sops.nix
@@ -1,11 +1,17 @@
 { inputs, pkgs, ... }:
 {
   flake.nixosModules.sops = { config, pkgs, ... }: {
+
+    imports = [ inputs.sops-nix.nixosModules.sops ];
+
     environment.systemPackages = with pkgs; [
       sops
-    ];     
-      
-    imports = [ inputs.sops-nix.nixosModules.sops ];
-    # TODO export SOPS_AGE_KEY_FILE="$HOME/.age/sops_admin"
+    ];
+
+    sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+    
+    environment.variables = {
+      SOPS_AGE_KEY_FILE = "/home/xam/.age/sops_admin";
+    };
   };
 }
