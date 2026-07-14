9ef654a69a0e418ff7eab2f9daf59294396897ee
xam <xam@example.com>
2026-07-14 20:44:29 +0200

    s

--- modules/system/services/lidarr.nix
+++ modules/system/services/lidarr.nix
@@ -3,10 +3,20 @@
   flake.nixosModules.lidarr = { inputs,... }: {
     imports = [ inputs.self.nixosModules.prowlarr ];
 
+    users.users.lidarr = {
+      isSystemUser = true;
+      group = "media";
+      home = "/var/lib/lidarr";     # App data directory
+      createHome = true;            # Auto-create the home folder
+      description = "Lidarr Service User";
+    };
+
+    users.groups.media = {};
+
     # Enable the Lidarr service
     services.lidarr = {
       enable = true;
-      user = "media";
+      user = "lidarr";
       group = "media";    
       openFirewall = true;  # Opens default port 8686
     };
