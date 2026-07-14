6cd6ab1b2f63ec7f60829688d8d247fe50128560
xam <xam@example.com>
2026-07-14 20:26:35 +0200

    lidarr

--- modules/system/core/ssh-authorized-keys.nix
+++ modules/system/core/ssh-authorized-keys.nix
@@ -1,6 +1,7 @@
 { ... }:
 {
   flake.nixosModules.ssh-authorized-keys = { ... }: {
+    nix.settings.trusted-users = [ "root" "xam" ];
     users.users.xam.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIYXIMoAZsZW5JwmIdNMjKBftTtHCkmYqjVRgfBSWfJH xam@hearth"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMw6u6BnzrwISM9w86T8ExyrsjhqG+N+GNWQGRaILd+n xam@sojourn"
