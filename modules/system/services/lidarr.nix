7fe996b5cf2395db7d2d085a6f85aac812459d4e
xam <xam@example.com>
2026-08-11 07:04:14 +0200

    fixed lidarr stuff

--- modules/system/services/lidarr.nix
+++ modules/system/services/lidarr.nix
@@ -31,8 +31,16 @@
       webuiPort = 8081;
       torrentingPort = 6881;
 
-      # This opens 8081 and 6881. See firewall note below.
+      # This opens 8081 and 6881.
       openFirewall = true;
     };
+
+    systemd.tmpfiles.rules = [
+      "d /mnt/media/downloads       		2775 qbittorrent media -"
+      "d /mnt/media/downloads/lidarr 		2775 qbittorrent media -"
+      "d /mnt/media/downloads/incomplete 	2775 qbittorrent media -"
+      
+      "d /mnt/media/music/lidarr     		2775 lidarr      media -"
+    ];
   };
 }
