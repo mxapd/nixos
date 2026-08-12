7611b53cea0491db1a70cf77aba27160f22bc73a
xam <xam@example.com>
2026-08-12 13:54:54 +0200

    conditional in sops

--- modules/system/core/sops.nix
+++ modules/system/core/sops.nix
@@ -10,7 +10,7 @@
       sops
     ];
 
-    sops.age.keyFile = "/home/xam/.age/sops_admin";
+    sops.age.keyFile =  lib.mkIf (lib.pathExists /home/xam/.age/sops_admin) "/home/xam/.age/sops_admin";
     sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 
   };
