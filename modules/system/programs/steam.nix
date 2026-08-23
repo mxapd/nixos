dddeab7af82b34fcad0956b73c39db1ba624133b
xam <xam@example.com>
2026-08-23 18:17:38 +0200

    Update 2026-08-23 18:17:38

--- modules/system/programs/steam.nix
+++ modules/system/programs/steam.nix
@@ -1,10 +1,14 @@
 { ... }: {
-  flake.nixosModules.steam = { ... }: {
+  flake.nixosModules.steam = { pkgs, ... }: {
     programs.steam = {
       enable = true;
       remotePlay.openFirewall = true;
       protontricks.enable = true;
     };
+
+    environment.systemPackages = with pkgs; [
+      steamtinkerlaunch
+    ];
 
     programs.gamemode.enable = true;
     programs.gamescope.enable = true;
