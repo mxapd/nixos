afd314884ed0932550792d733578c34f10410689
xam <xam@example.com>
2026-09-18 16:57:09 +0200

    a

--- modules/system/core/networking.nix
+++ modules/system/core/networking.nix
@@ -3,6 +3,7 @@
   flake.nixosModules.networking = { ... }: {
     networking = {
       networkmanager.enable = true;
+      nameservers = [ "1.1.1.1" "1.0.0.1" "8.8.8.8" "8.8.4.4" ];
       firewall.enable = true;
       firewall.allowedTCPPorts = [ ];
     };
